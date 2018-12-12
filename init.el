e298cda5245d62febf54ee619b66d4cc3a922dc4
Jocoo0326 <xzpnuist@gmail.com>
2018-12-12 11:51:01 +0800

    fix bugs

--- init.el
+++ init.el
@@ -5,7 +5,7 @@
 ;; (package-initialize)
 (require 'package)
 (setq package-archives '(("gnu" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
-			 ("melpa" . "http://mirros.tuna.tsinghua.edu.cn/elpa/melpa/")))
+			 ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
@@ -13,3 +13,25 @@
   (package-install 'use-package))
 
 (org-babel-load-file (concat user-emacs-directory "config.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("7e78a1030293619094ea6ae80a7579a562068087080e01c2b8b503b27900165c" "aaffceb9b0f539b6ad6becb8e96a04f2140c8faa1de8039a343a4f1e009174fb" "075351c6aeaddd2343155cbcd4168da14f54284453b2f1c11d051b2687d6dc48" "bd7b7c5df1174796deefce5debc2d976b264585d51852c962362be83932873d9" default)))
+ '(helm-gtags-auto-update t)
+ '(helm-gtags-ignore-case t)
+ '(helm-gtags-path-style (quote relative))
+ '(nil nil t)
+ '(package-selected-packages
+   (quote
+    (doom-themes dracula-theme sexy-monochrome-theme monokai-theme yasnippet-snippets which-key use-package try swiper spacemacs-theme spaceline nyan-mode neotree multiple-cursors magit helm-projectile helm-gtags ggtags flycheck expand-region evil company cnfonts avy all-the-icons ace-jump-mode)))
+ '(recentf-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
